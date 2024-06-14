779f63a3be91113fcb57ff8545fb7a28bcd4c893
Enes Kurbetoğlu <eneskurbetoglu1@gmail.com>
2024-06-14 10:18:05 +0300

    hide sync all button when no bulk url is saved

--- lib/screens/search_options/history_selection_screen.dart
+++ lib/screens/search_options/history_selection_screen.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:jsdict/providers/query_provider.dart";
 import "package:jsdict/screens/search_options/search_options_screen.dart";
+import "package:jsdict/singletons.dart";
 
 class HistorySelectionScreen extends SearchOptionsScreen {
   const HistorySelectionScreen({super.key})
@@ -18,6 +19,25 @@ class _HistorySelectionState extends State<_HistorySelection> {
   @override
   Widget build(BuildContext context) {
     final queryProvider = QueryProvider.of(context);
+    final List<Widget> syncWidgets =
+        getPreferences().getString("syncBulkUrl") != null
+            ? [
+                const VerticalDivider(
+                  thickness: 3,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    queryProvider.syncHistory();
+                    setState(() {});
+                  },
+                  child: const Text(
+                    "SYNC ALL",
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ]
+            : [];
 
     return SingleChildScrollView(
         child: Column(
@@ -39,20 +59,7 @@ class _HistorySelectionState extends State<_HistorySelection> {
                         color: Colors.blue, fontWeight: FontWeight.bold),
                   ),
                 ),
-                const VerticalDivider(
-                  thickness: 3,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    queryProvider.syncHistory();
-                    setState(() {});
-                  },
-                  child: const Text(
-                    "SYNC ALL",
-                    style: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.bold),
-                  ),
-                ),
+                ...syncWidgets
               ],
             ),
           ),
